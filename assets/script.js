0b1cd8bce0b204167c10e5bebbaaa9202f11b220
Joe Dodgson <Joe.Dodgson@arup.com>
2020-04-14 21:57:32 +0100

    Display current time and date. Apply styling based on current time

---
+++ assets/script.js
@@ -0,0 +1,41 @@
+// Declare variables
+var currentDay = $("#currentDay");
+var currentTime = $("#currentTime");
+
+// Declare functions
+
+function updateDisplay() {
+    setInterval(function(){
+        updateCurrentDay();
+        updateTimeSlotColour();
+    }, 500);
+}
+
+$(document).ready(function(){
+    updateDisplay();
+});
+
+// currentDay.value = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+
+
+function updateCurrentDay(){
+    var dayDateMonth = moment().format("ddd, Do MMMM YY");
+    var hrsMinsSecs = moment().format("h:mm:ss a");
+    currentDay.text(dayDateMonth);
+    currentTime.text(hrsMinsSecs);
+};
+
+function updateTimeSlotColour(){
+    $(".description").each(function(){
+        let id = parseInt($(this).parent().attr("id"));
+        // console.log(id);
+        $(this).removeClass("past present future");
+        if(id < moment().hour()){
+            $(this).addClass("past");
+        }else if(id === moment().hour()){
+            $(this).addClass("present");
+        }else{
+            $(this).addClass("future");
+        }
+    })
+};
