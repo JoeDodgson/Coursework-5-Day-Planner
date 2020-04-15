fef2ce9f2f46db8e95443c391f8e0c949c51660c
Joe Dodgson <Joe.Dodgson@arup.com>
2020-04-15 09:43:57 +0100

    Populated the README

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Coursework-5-Day-Planner
 Coding Bootcamp Coursework 5 - Day Planner
+
+A web page which lets the user enter and save text for each hour slot in their working day (9am-5pm).
+The page displays any text that the user previously saved.
+The page shows the current time and date. 
+Time slots change colour depending on the current time of day (grey = past, red = present, green = future).
+
+No installation required.
+
+To use this site, type whatever text you want into the coloured text area. Then click the save button on the right hand side to save that text.
+
+Contribution by Joe Dodgson only.
+
+Credits to my teachers at the Trilogy Coding Bootcamp (Chiggy and Steve).
+
+Open Source.
--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,11 @@ var currentDay = $("#currentDay");
 var currentTime = $("#currentTime");
 
 
-// Upon loading, update the time slot colours and start updating the display each half a second
-$(document).ready(function(){
-    updateTimeSlotColour();
-    renderDescriptions();
-    setInterval(function(){
-        updateCurrentDay();
-        updateTimeSlotColour();
-    }, 500);
-});
-
-
 // Function to apply classes to the time-slot description boxes, depending on the current time
-function updateTimeSlotColour(){
-
+function updateTimeSlotColour() {
+    
     // Go through each element with .description class
-    $(".description").each(function(){
+    $(".description").each(function() {
         
         // Parse the parent's ID to a number and store as blockHour (note parent div IDs are the hour)
         let blockHour = parseInt($(this).parent().attr("id"));
@@ -27,11 +16,11 @@ function updateTimeSlotColour(){
         $(this).removeClass("past present future");
         
         // Compare the blockHour against the current hour and apply the relevant styling
-        if(blockHour < moment().hour()){
+        if (blockHour < moment().hour()) {
             $(this).addClass("past");
-        }else if(blockHour === moment().hour()){
+        } else if (blockHour === moment().hour()) {
             $(this).addClass("present");
-        }else{
+        } else {
             $(this).addClass("future");
         }
     })
@@ -39,17 +28,17 @@ function updateTimeSlotColour(){
 
 
 // Function to display the descriptions saved in local storage
-function renderDescriptions(){
+function renderDescriptions() {
     
     // Go through each element with .description class
-    $(".description").each(function(){
+    $(".description").each(function() {
         
         // Store the parent's ID as hour (note parent div IDs are the hour)
         let hour = $(this).parent().attr("id");
-
+        
         // Retrieve the description from local storage, using the hour as the index
         hourDescription = localStorage.getItem(hour);
-
+        
         // Set the value of the description class to the description returned from local storage
         $(this).val(hourDescription);
     })
@@ -57,7 +46,7 @@ function renderDescriptions(){
 
 
 // Function to display the current date and time (at the top of the page) by pulling from moment.js
-function updateCurrentDay(){
+function updateCurrentDay() {
     var dayDateMonth = moment().format("ddd, Do MMMM YY");
     var hrsMinsSecs = moment().format("h:mm:ss a");
     currentDay.text(dayDateMonth);
@@ -72,4 +61,16 @@ $(".saveBtn").click(function() {
     
     // Data is stored locally as hour:description index:value pairs
     localStorage.setItem(hour,hourDescription);
+});
+
+
+// Upon loading, update the time slot colours and start updating the display each half a second
+$(document).ready(function() {
+    updateTimeSlotColour();
+    renderDescriptions();
+
+    setInterval(function() {
+        updateCurrentDay();
+        updateTimeSlotColour();
+    }, 500);
 });
